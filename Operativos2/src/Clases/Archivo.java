20e0b87bc5d06bc141e75a857a48fdca39c6c660
RGabo16 <131307512+RGabo16@users.noreply.github.com>
2025-11-07 10:51:19 -0400

    Add files via upload

---
+++ Operativos2/src/Clases/Archivo.java
@@ -0,0 +1,18 @@
+package Clases;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Archivo {
+    
+    String nombre;
+    int cantidad_bloq;
+    Bloque bloq_inicial;
+    
+}
